a8bfbd5623d17314be8567f3a30d85ef5e1918e0
RyanWilliams <ryanw@iastate.edu>
2013-07-24 13:06:51 -0500

    made and R scripts folder with adonis code in it

---
+++ R_stats_scripts/Untitled.R
@@ -0,0 +1,16 @@
+data<-read.csv(file.choose())
+names(data)
+
+library(vegan)
+
+#transform abundances for relative comparisons on columns that do not contain metadata
+enz.trans<-cbind(data[,1:3],decostand(data[,4:73],"total"))
+
+
+#the function is a y~x1+x2 format like this: community matrix~Trt1*Trt2
+adonis(enz.trans[,4:73]~enz.trans$Crop*enz.trans$SoilFrac)
+
+for nmds you can do it this way
+
+enz.mds<-metaMDS(enz.trans[,4:73])
+plot(enz.mds)
