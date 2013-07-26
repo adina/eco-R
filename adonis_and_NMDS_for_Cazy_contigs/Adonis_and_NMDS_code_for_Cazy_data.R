599493664ba7c2a81338f69bc24f5da68bb8cd7e
RyanWilliams <ryanw@iastate.edu>
2013-07-26 13:14:39 -0500

    folder with Cazy adonis and NMDS updated

--- adonis_and_NMDS_for_Cazy_contigs/Adonis_and_NMDS_code_for_Cazy_data.R
+++ adonis_and_NMDS_for_Cazy_contigs/Adonis_and_NMDS_code_for_Cazy_data.R
@@ -23,6 +23,29 @@ ggplot.NMDS(contig.nmds, data$SoilFrac, colors)
 
 #now to run the adonis
 
-adonis(contig.trans~data$SoilFrac, permutations=9999)
+adonis(contig.trans~data$SoilFrac, permutations=9999)### notsignficant 
+
+####Now performing analysis at Cazy_fam2, everything is summed at that cazy level within samples
+cazy.2.data<-read.csv("contigs_summed_at_cazy_fam_2.csv")
+head(cazy.2.data)
+dim(cazy.2.data)
+cazy.2.data$SoilFrac<-factor(cazy.2.data$SoilFrac, levels=c("Micro","SM","MM","LM","WS"))
+cazy.2.trans<-decostand(cazy.2.data[,3:8],"total")
+cazy.2.nmds<-metaMDS(cazy.2.trans,autotransform=FALSE)
+colors<-c("black","blue","purple","red","grey")
+ggplot.NMDS(cazy.2.nmds, cazy.2.data$SoilFrac, colors)
+adonis(cazy.2.trans~cazy.2.data$SoilFrac, permutations=9999)  ###not significant
+
+
+####Now performing analysis at Cazy_fam, everything is summed at that cazy level within samples
+cazy.data<-read.csv("contigs_summed_at_cazy_fam.csv")
+head(cazy.data)
+dim(cazy.data)
+cazy.data$SoilFrac<-factor(cazy.data$SoilFrac, levels=c("Micro","SM","MM","LM","WS"))
+cazy.trans<-decostand(cazy.data[,3:73],"total")
+cazy.nmds<-metaMDS(cazy.trans,k=3,autotransform=FALSE) #went to more k's, usually do this to keep stress under .1
+colors<-c("black","blue","purple","red","grey")
+ggplot.NMDS(cazy.nmds, cazy.data$SoilFrac, colors)
+adonis(cazy.trans~cazy.data$SoilFrac, permutations=9999)  ###not significant
 
 
